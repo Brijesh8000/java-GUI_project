b1f8e717c47e6b9c07341809a398f50aa1bf2527
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-01-30 23:43:40 +0530

    Add First

---
+++ Demo.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+
+public class Demo {
+    public static void main(String []args){
+        JFrame j1=new JFrame("Welcome to this page");
+        j1.setSize(500,500);
+        j1.setVisible(true);
+        j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+
+    }
+}
+
