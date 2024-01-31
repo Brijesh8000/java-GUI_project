f606025d1b82024005b0a8602bc6e332e165e813
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-01-31 21:54:11 +0530

    Add new Code in this file

--- java_basic_projest.java
+++ java_basic_projest.java
@@ -1,14 +1,31 @@
 // package java-GUI_project;
 import javax.swing.*;
+ class loginS extends JFrame {
+    loginS(String s1){
+        super(s1);
+    }
+    loginS(){
+
+    }
+    void setComponent(){
+        JFrame l1=new JFrame(" WELCOME THERE ");
+        JTextField t1= new JTextField();
+        setLayout(null);
+        t1.setBounds(200, 100, 100, 30);
+        t1.setBounds(200,200,100,30);
+        add(t1);
+        add(l1);
+    }    
+}
 
 public class java_basic_projest {
     public static void main(String []args){
-        JFrame j1=new JFrame("Welcome to this page");
-        j1.setSize(500,500);
-        j1.setVisible(true);
-        j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        loginS s1=new loginS("Welocome There ");
+       
+        s1.setVisible(true);
+        s1.setSize(700,700);
+        s1.setComponent();
 
-
+        s1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
