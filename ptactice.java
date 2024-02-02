129de68c03bc4f6a1c23c9ec0a76597525d570b4
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-02-02 23:46:53 +0530

    A

---
+++ ptactice.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+
+
+ class sol{
+    public static void main(String []arg){
+        
+        // int arr[5];
+        Scanner s1=new Scanner(System.in);
+        int n=s1.nextInt();
+        System.out.println(n);
+        // for(int i=0;i<arr.length;i++){
+        //     int arr[i]=s1.nextInt();
+
+
+        // }
+
+
+    }
+
+    
+}
+
+    
+
